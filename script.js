8c418be09a6074245bdb11a78f7ea657d04810b5
thehumanerror404 <thehumanerror404@proton.me>
2025-11-30 15:20:41 +0000

    lkjklj
    kjklj

--- script.js
+++ script.js
@@ -1,38 +1,46 @@
-function newgame() {
+let numbers = [];
 
-    var player = prompt("What do you choose, rock, paper or scissors?");
-    var options = ['rock','paper','scissors'];
-    var computer = options[Math.floor(Math.random() * 3)];
 
-    document.getElementById("computer-choice").innerText = "Computer chose: " + computer;    
+function generateArray() {
+    numbers = [];
 
-    if (player === computer) {
-        result = "Draw!";
+    for (let i = 0; i < 5; i++) {
+        numbers.push(Math.floor(Math.random() * 10) + 1);
     }
-    else if (player === "rock") {
-        if (computer === "scissors") {
-            result = "You win!";
-        } else {
-            result = "You lose!";
+
+    
+    document.getElementById("arrayOutput").style.display = "none";
+    document.getElementById("result").innerText = "";
+}
+
+function checkGuess() {
+    let guess = parseInt(document.getElementById("guess").value);
+
+    if (numbers.length === 0) {
+        document.getElementById("result").innerText =
+            "You must generate the array first!";
+        return;
+    }
+
+    if (isNaN(guess) || guess < 1 || guess > 10) {
+        document.getElementById("result").innerText =
+            "Please enter a valid number (1–10)";
+        return;
+    }
+
+    let count = 0;
+
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] === guess) {
+            count++;
         }
     }
-    else if (player === "paper") {
-        if (computer === "rock") {
-            result = "You win!";
-        } else {
-            result = "You lose!";
-        }
-    }
-    else if (player === "scissors") {
-        if (computer === "paper") {
-            result = "You win!";
-        } else {
-            result = "You lose!";
-        }
-    }
-    else {
-        result = "That's not an option!";
-    }
 
-    document.getElementById("result").innerText = result;
+  
+    document.getElementById("result").innerText =
+        "Your number appears " + count + " time(s).";
+
+    document.getElementById("arrayOutput").innerText =
+        "Generated Array: " + numbers.join(", ");
+    document.getElementById("arrayOutput").style.display = "block";
 }
